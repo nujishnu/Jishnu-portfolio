5efd76ced16965af9a1cf5a1fcc74538e0ef0015
nujishnu <nujishnu@gmail.com>
2025-02-13 00:36:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("Portfolio site loaded!");
+    let sections = document.querySelectorAll("section");
+    sections.forEach(sec => {
+        sec.style.opacity = "0";
+        sec.style.transition = "opacity 1.5s ease-in-out";
+        setTimeout(() => sec.style.opacity = "1", 500);
+    });
 });
